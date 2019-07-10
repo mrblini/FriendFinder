7f32f87140456f6fe9ba8b01b599dcc4cb4582d7
pablodiazt <pablodiaztr@gmail.com>
2019-07-10 09:12:28 -0500

    changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,37 +1,40 @@
 module.exports = [];
 
-
-{
-    "name": "Johnny McArony",
+var friends = [
+    {
+        "name": "Johnny McArony",
         "photo": "https://www.google.com/search?q=ultimate+alpha+male&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjU28e-9qfjAhUQB50JHa_DArsQ_AUIECgB&biw=1440&bih=788&dpr=2#imgrc=jq1RFt3nDW0OMM:",
-            "scores": [
-                5,
-                1,
-                4,
-                4,
-                5,
-                1,
-                2,
-                5,
-                4,
-                1
-            ]
-}
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
 
-{
-    "name": "Dr. Johnny",
+    {
+        "name": "Dr. Johnny",
         "photo": "https://www.google.com/search?q=ultimate+alpha+male&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjU28e-9qfjAhUQB50JHa_DArsQ_AUIECgB&biw=1440&bih=788&dpr=2#imgrc=MzwGTeqmAQXf0M:",
-            "scores": [
-                5,
-                1,
-                4,
-                4,
-                5,
-                1,
-                2,
-                5,
-                4,
-                1
-            ]
-}
+        "scores": [
+            2,
+            1,
+            4,
+            4,
+            3,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+]
+
+
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,25 +1,14 @@
+
 var friends = require('../data/friends.js');
 
 module.exports = function (app) {
     // get api/friends route
-    app.get('friends', function (req, res) {
+    app.get('/api/friends', function (req, res) {
         return res.json(friends);
     });
 
     // post api/friends route
-    app.post('friends', function (req, res) {
+    app.post('/api/friends', function (req, res) {
         friends.push(req.body);
     });
 };
-
-// // Displays a single friend, or returns false
-    // app.get("/api/friends/:friend", function(req, res) {
-    //     var chosen = req.params.friend;
-
-    //     for (var i = 0; i < friends.length; i++) {
-    //         if (chosen === friends[i].routeName) {
-    //         return res.json(friends[i]);
-    //         }
-    //     }
-    // return res.json(false);
-    // });
